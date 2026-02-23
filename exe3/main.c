7077212033cf29dc7f44aee573907e22a813d1d2
fefebiru <felipet4@al.insper.edu.br>
2026-02-23 20:59:07 -0300

    ex3 v2

--- exe3/main.c
+++ exe3/main.c
@@ -1,54 +1,54 @@
-#include "hardware/gpio.h"
-#include "pico/stdlib.h"
 #include <stdio.h>
 
-const int LED_PIN_R = 4;
-const int LED_PIN_G = 6;
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
 
-const int BTN_PIN_R = 28;
-const int BTN_PIN_G =26;
+static const int LED_PIN_R = 4;
+static const int LED_PIN_G = 6;
 
+static const int BTN_PIN_R = 28;
+static const int BTN_PIN_G = 26;
 
-int main() {
-  stdio_init_all();
+int main(void) {
+    stdio_init_all();
 
-  gpio_init(LED_PIN_R);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
-  gpio_init(LED_PIN_G);
-  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
 
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
 
-  gpio_init(BTN_PIN_G);
-  gpio_set_dir(BTN_PIN_G, GPIO_IN);
-  gpio_pull_up(BTN_PIN_G);
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
 
-  int led_r = 0;
-  int led_g = 0;
+    int led_r = 0;
+    int led_g = 0;
 
-  while (true) {
-    if (!gpio_get(BTN_PIN_R)) {
-      if (led_r == 0) {
-        led_r = 1;
-      } else {
-        led_r = 0;
-      }
-      gpio_put(LED_PIN_R, led_r);
-      while (!gpio_get(BTN_PIN_R)) {
-      };
+    int last_r = 1; // pull-up => solto = 1
+    int last_g = 1; // pull-up => solto = 1
+
+    while (true) {
+        int cur_r = gpio_get(BTN_PIN_R);
+        int cur_g = gpio_get(BTN_PIN_G);
+
+        // Detecta borda de descida: 1 -> 0 (apertou)
+        if (last_r && !cur_r) {
+            led_r = !led_r;
+            gpio_put(LED_PIN_R, led_r);
+        }
+
+        if (last_g && !cur_g) {
+            led_g = !led_g;
+            gpio_put(LED_PIN_G, led_g);
+        }
+
+        last_r = cur_r;
+        last_g = cur_g;
+
     }
-    if (!gpio_get(BTN_PIN_G)) {
-      if (led_g == 0) {
-        led_g = 1;
-      } else {
-        led_g = 0;
-      }
-      gpio_put(LED_PIN_G, led_g);
-      while (!gpio_get(BTN_PIN_G)) {
-      };
-    }
-  }
 }
