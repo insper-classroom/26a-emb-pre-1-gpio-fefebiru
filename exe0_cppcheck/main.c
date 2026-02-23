60f08ec8f182aa03519cda1d388e236cf97c871f
fefebiru <felipet4@al.insper.edu.br>
2026-02-23 19:27:37 -0300

    ex0 cppcheck v2

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -5,6 +5,7 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
@@ -12,7 +13,7 @@ int main(void) {
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
         printf("Element %d: %d\n", i, arr[i]);
     }
